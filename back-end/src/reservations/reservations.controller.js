8049b9ce42dad17f47b2984fe433084e6106ab0f
Lorraine <dmjlorraine@gmail.com>
2024-01-31 16:07:34 -0600

    list function for reservations controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,12 +4,15 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+async function list(req, res, next) {
+  try {
+    const data = await service.list();
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
