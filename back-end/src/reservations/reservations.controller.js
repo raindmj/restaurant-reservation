c892990220cdd5b7625d7352411ba6b861eaa7a0
Lorraine <dmjlorraine@gmail.com>
2024-02-02 02:30:53 -0600

    list reservations partially functioning

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,17 +1,80 @@
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+
+/*
+ * Validation middleware
+ */
+
+async function reservationExists(req, res, next) {
+  const { reservation_id } = req.params;
+  const reservation = await service.read(reservation_id);
+  if (reservation) {
+    res.locals.reservation = reservation;
+    return next();
+  } else {
+    next({
+      status: 404,
+      message: `Reservation with id ${reservation_id} does not exist.`,
+    });
+  }
+}
+
+/* function asDateString(date) {
+  return `${date.getFullYear().toString(10)}-${(date.getMonth() + 1)
+    .toString(10)
+    .padStart(2, "0")}-${date.getDate().toString(10).padStart(2, "0")}`;
+}
+
+function todaysDate() {
+  return asDateString(new Date());
+}
+
+console.log(todaysDate()); */
+
+function todaysDate2() {
+  const date = new Date();
+
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+
+  const formattedDate = year + "-" + month + "-" + day;
+
+  return formattedDate;
+}
+
+// console.log(todaysDate2());
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res, next) {
   try {
-    const data = await service.list();
-    res.json({ data });
+    const { date, mobile_number } = req.query;
+    console.log(date);
+    console.log(mobile_number);
+    // list by date
+    if (date) {
+      const data = await service.listByDate(date);
+      res.json({ data });
+      // list by mobile number
+    } else if (mobile_number) {
+      const data = await service.search(mobile_number);
+      res.json({ data });
+      // list by today's date
+    } else {
+      const data = await service.list();
+      res.json({ data });
+    }
   } catch (error) {
     next(error);
   }
 }
+
+/*
+ * Read a reservation given reservation_id
+ */
 
 module.exports = {
   list: asyncErrorBoundary(list),
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,11 +1,36 @@
 const knex = require("../db/connection");
 
 // list all of the information from reservations table
-// reservation_id, created_at, updated_at, first_name, last_name, mobile_number, reservation_date, reservation_time, people
+// reservation_id, created_at, updated_at, first_name, last_name, mobile_number, reservation_date, reservation_time, people, status
 function list() {
-  return knex("reservations").select("*");
+  return knex("reservations")
+    .select("*")
+    .whereNotIn("status", ["finished", "cancelled"])
+    .orderBy("reservation_time");
+}
+
+// list all of the info for a given date
+function listByDate(reservationDate) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_date: reservationDate })
+    .whereNotIn("status", ["finished", "cancelled"])
+    .orderBy("reservation_time");
+}
+
+// search reservations by mobile_number
+// ignore all formatting and search only for the digits
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
 }
 
 module.exports = {
   list,
+  listByDate,
+  search,
 };
