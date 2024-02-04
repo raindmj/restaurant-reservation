6645b342a93731cf9a00de300e9543e0ada039b0
Lorraine <dmjlorraine@gmail.com>
2024-02-04 16:49:48 -0600

    order list tables by table name

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -3,7 +3,7 @@ const knex = require("../db/connection");
 // list all of the information from reservations table
 //
 function list() {
-  return knex("tables").select("*");
+  return knex("tables").select("*").orderBy("table_name");
 }
 
 module.exports = {
